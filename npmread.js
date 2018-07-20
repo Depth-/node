c44c73e7b53fe1a545073a103141bf22f88e4de9
wwwer52@gmail.com <wwwer52@gmail.com>
2018-07-20 17:48:05 +0800

    更新

---
+++ npmread.js
@@ -0,0 +1,34 @@
+//npm root 安装路径
+//npm root -g 全局路径
+//npm ls 查看安装包
+//npm install name -g 全局安装 注意权限
+//npm update 更新
+//npm remove 删除
+//npm init -f 建立一个空的模板
+
+//编码规范
+//  双空格缩进
+//每一个函数，必须定义var
+function ( ){ //大括号方式
+  var a=1; // var 函数变量声明
+  a = a + c; // 算法必须加空格
+}
+
+function add(){
+  var a = 1,
+      b = 2;
+  return;
+      a + b;
+}
+
+//name
+//变量命名 adminUser passWord 先小写后大写 不允许_连接
+//方法命名 getStudentId returnUserInfo 动词加大写名字
+//类命名   UserDatabase 首字母大写
+//常量命名 FROM_NAME 全大写 可用用下划线
+//包命名   不要用js node
+
+//异步 状态加数据
+function sss(err,data){
+  return;
+}
--- test.js
+++ test.js
@@ -3,3 +3,5 @@ for (var i = 2; i >=0; i--) {
 	debug("Sublime Test3:"+i);
 }
 debug(temp);
+// 优点
+// 适合高并发 不 是和高数据
